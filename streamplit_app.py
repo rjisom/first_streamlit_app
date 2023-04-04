d9238165195a3dcc9b8d29ef01f0cc6332769c89
whoami <rich.john.isom@gmail.com>
2023-04-04 13:59:47 -0700

    Update streamplit_app.py
    function to query data

--- streamplit_app.py
+++ streamplit_app.py
@@ -50,13 +50,20 @@ except URLError as e:
 
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
+
 streamlit.header("Fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+
+# Snowflake fucnitons
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.excecute('SELECT * FROM fruit_load_list')
+    return my_cur.fetchall()
+  
+#button to load fruit
+if streamlit.button('Get Fruit load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 # Search for fruits dynamically
 fruit_to_add = streamlit.text_input('What fruit would you like to add?')
