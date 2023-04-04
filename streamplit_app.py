c9c8a1bdb7ffd7ac5b379746552e99a0b516f768
whoami <rich.john.isom@gmail.com>
2023-04-04 12:37:56 -0700

    Update streamplit_app.py
    fixed insert with value

--- streamplit_app.py
+++ streamplit_app.py
@@ -50,7 +50,7 @@ streamlit.dataframe(my_data_rows)
 
 # Search for fruits dynamically
 fruit_to_add = streamlit.text_input('What fruit would you like to add?')
-#my_cur.execute("INSERT " + fruit_to_add + " into fruit_load_list")
-my_data_rows.append(fruit_to_add)
+my_cur.execute("INSERT into fruit_load_list " + "values ("+fruit_to_add+")")
+#my_data_rows.append(fruit_to_add)
 streamlit.write('Thanks for adding ', fruit_to_add)
 
