7400578c105559ed6b2d7af5fa21401212542c21
whoami <rich.john.isom@gmail.com>
2023-04-04 10:42:04 -0700

    Update streamplit_app.py
    added fruityvice to data frame for better readability

--- streamplit_app.py
+++ streamplit_app.py
@@ -30,3 +30,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize data in pandas dataframe
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# wdisplay dataframe
+streamlit.dataframe(fruityvice_normalized)
