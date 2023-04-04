791b27113211ab26a152a8436143ee4f6521aa49
whoami <rich.john.isom@gmail.com>
2023-04-04 13:53:04 -0700

    Update streamplit_app.py
    error check and function to get fruit data

--- streamplit_app.py
+++ streamplit_app.py
@@ -27,6 +27,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  # normalize data in pandas dataframe
+  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 #fruityvice api
 streamlit.header("Fruityvice Fruit Advice!")
 try: 
@@ -35,11 +41,9 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    # normalize data in pandas dataframe
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    back_from_function = get_fruityvice_data(fruit_choice)
     # wdisplay dataframe
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
  
 except URLError as e:
   streamlit.error()
