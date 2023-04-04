4b744bd8d2813b3d91dbe81dcd95290690707ce9
whoami <rich.john.isom@gmail.com>
2023-04-03 19:26:40 -0700

    Update streamplit_app.py
    Added headers and text

--- streamplit_app.py
+++ streamplit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('First Snowflake Streamlit App')
+
+streamlit.header('This is a streamlist header')
+
+streamlit.text('Some streamlit Text')
+
+streamlit.test('Some more streamlit text')
