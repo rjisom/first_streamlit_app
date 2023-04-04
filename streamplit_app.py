42a478b7d71dbe1108db2aa8b6697db740af28c6
whoami <rich.john.isom@gmail.com>
2023-04-04 13:22:15 -0700

    Update streamplit_app.py
    check if fruit is empty

--- streamplit_app.py
+++ streamplit_app.py
@@ -51,10 +51,12 @@ streamlit.dataframe(my_data_rows)
 # Search for fruits dynamically
 fruit_to_add = streamlit.text_input('What fruit would you like to add?')
 
-my_cur.execute("INSERT into fruit_load_list values "+f"('{fruit_to_add}')")
+if (len(fruit_to_add) != 0):
+  my_cur.execute("INSERT into fruit_load_list values "+f"('{fruit_to_add}')")
+  streamlit.write('Thanks for adding ', fruit_to_add)
 #.execute("... WHERE my_column = %(name)s", {"name": value})
 
 #my_data_rows.append(fruit_to_add)
 
-streamlit.write('Thanks for adding ', fruit_to_add)
+
 
