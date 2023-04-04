9da7f74ac4a700b47aa6c649a3eaa8506c39f080
whoami <rich.john.isom@gmail.com>
2023-04-04 10:43:44 -0700

    Update streamplit_app.py
    added fruit choice box

--- streamplit_app.py
+++ streamplit_app.py
@@ -26,10 +26,13 @@ streamlit.dataframe(fruits_to_show)
 
 #fruityvice api
 streamlit.header("Fruityvice Fruit Advice!")
+# Search for fruits dynamically
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 # normalize data in pandas dataframe
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
