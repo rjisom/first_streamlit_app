148854a6838eaf3084433fa7dea103223cf6cdc2
whoami <rich.john.isom@gmail.com>
2023-04-04 12:11:27 -0700

    Update streamplit_app.py
    added select statement from snowflake for fruit list. cleaned up fruit list displayed as streamlit.dataframe

--- streamplit_app.py
+++ streamplit_app.py
@@ -2,13 +2,6 @@ import streamlit
 import pandas as pd
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -46,3 +39,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # wdisplay dataframe
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * FROM fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.header("Fruit load list contains:")
+streamlit.dataframe(my_data_row)
+
