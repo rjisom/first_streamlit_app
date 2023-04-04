682587fe525e24ec7702da9335334abfedc4f692
whoami <rich.john.isom@gmail.com>
2023-04-04 14:23:57 -0700

    Update streamplit_app.py
    added close to 2nd connection

--- streamplit_app.py
+++ streamplit_app.py
@@ -76,6 +76,7 @@ fruit_to_add = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(fruit_to_add)
+  my_cnx.close()
   streamlit.text(back_from_function)
   
 streamlit.stop() 
