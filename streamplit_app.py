a89f7ce89011b8fc3d8cc0d6958929ae474d4519
whoami <rich.john.isom@gmail.com>
2023-04-04 12:46:55 -0700

    Update streamplit_app.py
    added curly brackets for python string to query properly

--- streamplit_app.py
+++ streamplit_app.py
@@ -50,8 +50,8 @@ streamlit.dataframe(my_data_rows)
 
 # Search for fruits dynamically
 fruit_to_add = streamlit.text_input('What fruit would you like to add?')
-fruit_to_add = "'"+fruit_to_add+"'"
-my_cur.execute("INSERT into fruit_load_list " + "values ("+fruit_to_add+")")
+
+my_cur.execute("INSERT into fruit_load_list " + "values ("{fruit_to_add}")")
 #my_data_rows.append(fruit_to_add)
 streamlit.write('Thanks for adding ', fruit_to_add)
 
