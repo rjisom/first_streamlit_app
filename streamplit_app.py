d99d856249d5a05700d26bee8dc58c93424016f2
whoami <rich.john.isom@gmail.com>
2023-04-03 19:31:18 -0700

    Update streamplit_app.py
    Added pandas and csv with dataframe

--- streamplit_app.py
+++ streamplit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('First Snowflake Streamlit App')
 
@@ -11,3 +12,7 @@ streamlit.text('Some more streamlit text')
 streamlit.text('Look! emojis! 🥣 🥗 🐔 🥑🍞')
 
 streamlit.header('🍌🥭 Fruit Header! 🥝🍇')
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+streamlit.dataframe(my_fruit_list)
